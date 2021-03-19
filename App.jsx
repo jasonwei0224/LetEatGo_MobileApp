0119d4d7ae80acbfbc5bf7da1fc5046e5c9490b9
jasonwei0224 <45651276+jasonwei0224@users.noreply.github.com>
2021-03-18 21:23:45 -0700

    add input

--- App.jsx
+++ App.jsx
@@ -2,11 +2,13 @@ import { StatusBar } from "expo-status-bar";
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
 import Button from "./src/components/Button";
+import FormTextInput from "./src/components/FormTextInput";
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text> Hi </Text>
+      <FormTextInput></FormTextInput>
       <Button></Button>
       <StatusBar style="auto" />
     </View>
--- src/components/FormTextInput.tsx
+++ src/components/FormTextInput.tsx
@@ -1,1 +1,22 @@
 
+import React from "react"; 
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import { TextInput } from "react-native-gesture-handler";
+
+
+export default function FormTextInput(){
+    return(
+        <TextInput
+        style={styles.input}
+            />
+        
+    );
+}
+const styles = StyleSheet.create({
+    input: {
+        alignItems: "center",
+        backgroundColor: "#DDDDDD",
+        padding: 10
+      },
+  });
+  
