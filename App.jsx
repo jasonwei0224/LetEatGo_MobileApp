5683a2c053f054b57ba0ae6e6fd819ca99a2f1ee
sangwookYun <brtm0533@naver.com>
2021-03-21 14:39:52 -0700

    navbar

--- App.jsx
+++ App.jsx
@@ -2,14 +2,37 @@ import { StatusBar } from "expo-status-bar";
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
 import Button from "./src/components/Button";
+import {NavigationContainer} from "@react-navigation/native";
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs'
+// import UserScreen from './src/screens/UserScreen'
+// import TestScreen from './src/screens/TestScreen'
 
+
+function UserScreen() {
+  return (
+    <View>
+      <Text> This is a User Screen</Text>
+    </View>
+  )
+}
+
+function TestScreen() {
+  return (
+    <View>
+      <Text> This is a test Screen</Text>
+    </View>
+  )
+}
+const Tab = createBottomTabNavigator();
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text> Hi </Text>
-      <Button></Button>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+    <Tab.Navigator>
+      <Tab.Screen name = "home" component = {UserScreen}></Tab.Screen>
+      <Tab.Screen name = "User" component = {TestScreen}></Tab.Screen>
+    </Tab.Navigator>
+    </NavigationContainer>
+    
   );
 }
 
