f2adcc8ac8ce83ec850b498f4dadf4adc3639b4f
sangwookYun <brtm0533@naver.com>
2021-03-25 22:46:03 -0700

    axios call done

--- src/screens/HomeScreen.jsx
+++ src/screens/HomeScreen.jsx
@@ -1,35 +1,47 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import {FlatList, View, Text, StyleSheet, Image} from 'react-native';
+import axios from 'axios'
 
 const HomeScreen = () => {
-    const dataSet = [
-        {key: 1, name : "SWEET MEMORY", addr: "7835 13th Ave, Burnaby", desc : "#Brunch #Egg"},
-        {key: 2, name : "Ahn and Chi", addr: "135 7th Ave, Vancouver", desc : "#Cafe # study"},
-        {key: 3, name : "Milkcow Cafe", addr: "15325 110A, Surrey", desc : "#good # nice View"},
-        {key: 4, name : "Hiel Cafe", addr: "110, North Van", desc : "#delicious #Food"},
-        {key: 5, name : "Busan DAEJI", addr: "100 Cambie St, Vancouver", desc : "#very #damm"},
-        {key: 6, name : "The Keg steak", addr: "Langley, Vancouver", desc : "#super #tired"}
-    ]
+    // const dataSet = [
+    //     {key: 1, name : "SWEET MEMORY", addr: "7835 13th Ave, Burnaby", desc : "#Brunch #Egg"},
+    //     {key: 2, name : "Ahn and Chi", addr: "135 7th Ave, Vancouver", desc : "#Cafe # study"},
+    //     {key: 3, name : "Milkcow Cafe", addr: "15325 110A, Surrey", desc : "#good # nice View"},
+    //     {key: 4, name : "Hiel Cafe", addr: "110, North Van", desc : "#delicious #Food"},
+    //     {key: 5, name : "Busan DAEJI", addr: "100 Cambie St, Vancouver", desc : "#very #damm"},
+    //     {key: 6, name : "The Keg steak", addr: "Langley, Vancouver", desc : "#super #tired"}
+    // ]
+    const [data, setData] = useState({})
+
+    useEffect(() => {
+        axios.get("https://leg-backend.herokuapp.com/api/v1/restaurant").then((res)=> {
+            console.log(res.data['data'])
+            setData(res.data['data'])
+        }).catch((err)=> {
+            console.log(err)
+        })
+    })
     return (
         <View style={styles.container}>
             <View style = {styles.case1}><Text></Text></View>
             <View style = {styles.case2}><Text></Text></View>
             <View style = {styles.case3}>
                 <FlatList 
-                    data = {dataSet}
+                    data = {data}
                     renderItem = {({item})=>
                         <View style = {styles.case4}>
                             <View style = {styles.imageWrapper}>
                                 <Image
-                                    source = {require('../../image/logo.png')}
+                                    // source = {require('../../image/logo.png')}
+                                    source = {{uri:item.mainPicture}}
                                     style = {styles.logo}>
                                 </Image>
                             </View>
                        
                             <View style = {styles.case5}>
                                 <Text style= {styles.name}>{item.name}</Text>
-                                <Text style= {styles.addr}>{item.addr}</Text>
-                                <Text style= {styles.desc}>{item.desc}</Text>
+                                <Text style= {styles.addr}>{item.address}</Text>
+                                <Text style= {styles.desc}>{item.mainTitle}</Text>
                             </View>
                            
                         </View>}
